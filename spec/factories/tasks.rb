a985819494312b6586ce3d8930269321713d22ab
川原じゅん <kawaharajun@kawaharajunnoMacBook-2.local>
2021-02-26 09:58:12 +0900

    Myself:RSpec2

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :task do
-    
+    title { "test-title" }
+    content { "test-content" }
+    status { 0 }
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'タイトルがない場合はエラー' do
+      task = FactoryBot.build(:task, title: nil)
+      task.valid?
+      expect(task.errors[:title]).to include("can't be blank")
+    end
+
+    it 'タイトルが重複している場合はエラー' do
+      FactoryBot.create(:task, title: "test")
+      task = FactoryBot.build(:task, title: "test")
+      task.valid?
+      expect(task.errors[:title]).to include("has already been taken")
+    end
+
+    it 'ステータスがない場合はエラー' do
+      task = FactoryBot.build(:task, status: nil)
+      task.valid?
+      expect(task.errors[:status]).to include("can't be blank")
+    end
+  end
 end
