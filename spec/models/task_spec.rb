cc05d0785569f269d26ad325e6ff2225b741748e
川原じゅん <kawaharajun@kawaharajunnoMacBook-2.local>
2021-02-26 15:56:21 +0900

    Fix:Rspec2

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe 'validation' do
+    it '全てのカラムが正しい場合は正常処理' do
+      task = FactoryBot.build(:task)
+      expect(task).to be_valid
+    end
+
     it 'タイトルがない場合はエラー' do
       task = FactoryBot.build(:task, title: nil)
       task.valid?
@@ -13,6 +18,12 @@ RSpec.describe Task, type: :model do
       task = FactoryBot.build(:task, title: "test")
       task.valid?
       expect(task.errors[:title]).to include("has already been taken")
+    end
+
+    it 'タイトルが別の場合は正常処理' do
+      FactoryBot.create(:task, title: "test")
+      task = FactoryBot.build(:task, title: "test2")
+      expect(task).to be_valid
     end
 
     it 'ステータスがない場合はエラー' do
