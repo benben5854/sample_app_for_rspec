357436b95e211304f1bb7c5e5545c1517f083a9b
川原じゅん <kawaharajun@kawaharajunnoMacBook-2.local>
2021-03-03 19:35:56 +0900

    FIX2:RSpec03

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :system do
+  let(:user) { create(:user) }
 
   describe 'ログイン前' do
     context 'フォームの入力値が正常' do
       it 'ログイン処理が成功する' do
-        user = FactoryBot.create(:user)
         
         visit login_path
         fill_in "Email", with: user.email
@@ -17,7 +17,6 @@ RSpec.describe "UserSessions", type: :system do
     end
     context 'フォームが未入力' do
       it 'ログイン処理が失敗する' do
-        user = FactoryBot.create(:user)
         visit login_path
         fill_in "Email", with: ""
         fill_in "Password", with: ""
@@ -32,7 +31,6 @@ RSpec.describe "UserSessions", type: :system do
   describe 'ログイン後' do
     context 'ログアウトボタンをクリック' do
       it 'ログアウト処理が成功する' do
-        user = FactoryBot.create(:user)
         visit login_path
         fill_in "Email", with: user.email
         fill_in "Password", with: "123456"
